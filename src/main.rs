35b0173038d9ad322ea43a2a4ff93183582e243c
Marat Amirov <amirov@secretgroup.ru>
2021-10-09 22:47:00 +0200

    added additional fields to webhook payload

--- src/main.rs
+++ src/main.rs
@@ -25,15 +25,20 @@ fn main() {
 fn parse_json(request: Cow<str>) -> String {
     let mut json: String = String::new();
     let mut append = false;
+    let mut count = 0;
     for c in request.chars().into_iter() {
         if c.to_string().eq("{") {
             json += &c.to_string();
+            count += 1;
             append = true;
             continue
         }
         if c.to_string().eq("}") {
             json += &c.to_string();
-            append = false;
+            if count == 1 {
+                append = false;
+            }
+            count -= 1;
             continue;
         }
         if append {
--- src/webhook.rs
+++ src/webhook.rs
@@ -3,12 +3,25 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct WebHook {
     event_type: String,
-    pub user: User
+    pub user: User,
+    pub object_attributes: Option<ObjectAttr>
 }
 
 #[derive(Deserialize)]
 pub struct User {
-    id: i64,
-    username: String,
+    pub id: i64,
+    pub username: String,
     pub name: String
+}
+
+#[derive(Deserialize)]
+pub struct ObjectAttr {
+    pub id: Option<i64>,
+    pub target_branch: Option<String>,
+    pub source_branch: Option<String>,
+    pub state: Option<String>,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub url: Option<String>,
+    pub action: Option<String>
 }
