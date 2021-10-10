67811aff84616425d495089e008c8326b086720f
Marat Amirov <amirov@secretgroup.ru>
2021-10-10 12:13:08 +0200

    deleted large reqwest lib, refactored to tcpstream

--- src/telegram_client.rs
+++ src/telegram_client.rs
@@ -1,25 +1,40 @@
 use std::env;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::ops::Add;
+use native_tls::TlsConnector;
 
 pub struct TelegramClient {
     pub api_key: String
 }
 
 impl TelegramClient {
-    const URL: &'static str = "https://api.telegram.org/";
+    const TCP_ADDR: &'static str = "api.telegram.org:443";
+    const HOST: &'static str = "api.telegram.org";
     const GET_UPDATE: &'static str = "/getUpdates";
     fn get_url(&self) -> String {
-        format!("{}{}{}", TelegramClient::URL, "bot", &self.api_key)
+        format!("{}{}{}", TelegramClient::HOST, "bot", &self.api_key)
     }
     pub fn send_message(&self) {
 
     }
-    pub fn get_update(&self) {
-        let response = reqwest::blocking::get(format!("{}{}", &self.get_url(), TelegramClient::GET_UPDATE)).unwrap().text().unwrap();
-        println!("Body:\n {}", response);
-    }
 
     pub fn add_update_listener<F: Fn(String)>(&self, func: F) {
-        let response = reqwest::blocking::get(format!("{}{}", &self.get_url(), TelegramClient::GET_UPDATE)).unwrap().text().unwrap();
-        func(response)
+        let connector = TlsConnector::new().unwrap();
+        let stream = TcpStream::connect(TelegramClient::TCP_ADDR).unwrap();
+        let mut stream = connector.connect("api.telegram.org", stream).unwrap();
+        let mut request = String::new();
+        request.push_str(&format!("GET /{}{}{} HTTP/1.1\r\n", "bot", &self.api_key, TelegramClient::GET_UPDATE));
+        request.push_str(&format!("Host: {}\r\n", TelegramClient::HOST));
+        request.push_str("Accept: application/json\r\n");
+        request.push_str("Connection: close\r\n\r\n");
+        println!("Request {}", request);
+        stream.write(request.as_bytes());
+        stream.flush();
+
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        let request = String::from_utf8_lossy(&buffer[..]);
+        func(request.to_string())
     }
 }
